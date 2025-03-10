8e088f11a54a0b11b7b7b45ae707baa22f300644
24LP8639 24LP8639 <24LP8639@G305-employeeiMac.local>
2025-03-10 18:26:35 +0800

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className=" flex">
-      <ClientCard />
-      <ClientCard />
-      <ClientCard />
-      <ClientCard />
-      <ClientCard />
-      <ClientCard />
-      <ClientCard />
-    </div>
-  );
+  return <div className=" flex"></div>;
 }
