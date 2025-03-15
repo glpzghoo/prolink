51de794fd0b184c07a944dc78db1608bc96cc677
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-03-15 08:24:50 +0800

    a

--- src/app/job/[id]/page.tsx
+++ src/app/job/[id]/page.tsx
@@ -26,7 +26,7 @@ type CustomReviewee = review & {
   reviewee: CustomUser;
   reviewer: CustomUser;
 };
-export const calculateTime = (data: string) => {
+const calculateTime = (data: string) => {
   const timeago = (new Date().getTime() - new Date(data).getTime()) / 1000;
   if (timeago / 60 / 60 / 24 > 1) {
     return Math.round(timeago / 60 / 60 / 24) + " хоногийн өмнө";
