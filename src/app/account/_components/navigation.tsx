cd795f243247e6f21ffee87d5f899efeed346752
24LP0846 24LP0846 <24LP0846@G302-CC0089.local>
2025-03-19 14:31:41 +0800

    up

--- src/app/account/_components/navigation.tsx
+++ src/app/account/_components/navigation.tsx
@@ -10,125 +10,133 @@ import { RiLogoutBoxRFill } from "react-icons/ri";
 import { responseData } from "@/lib/types";
 import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@mui/material";
+import { userInfo } from "os";
 export function Navigation() {
-  const [response, setUserInfo] = useState<responseData>();
-  const [loading, setLoading] = useState(true);
+	const [response, setUserInfo] = useState<responseData>();
+	const [loading, setLoading] = useState(true);
 
-  const pathname = usePathname();
-  const getInfo = async () => {
-    const res = await axios.get(`/api/account`);
-    setUserInfo(res.data);
-    await axios.get(`/api/account/refreshToken`);
-  };
-  useEffect(() => {
-    try {
-      getInfo();
-    } catch (err) {
-      console.error(err, "Сервертэй холбогдож чадсангүй!");
-    } finally {
-      setLoading(false);
-    }
-  }, [pathname]);
-  const logout = async () => {
-    setLoading(true);
-    setUserInfo(undefined);
-    const res = await axios.get(`/api/account/logout`);
-    setUserInfo(res.data);
-    setLoading(false);
-  };
-  return (
-    <div className="flex justify-around items-center py-4">
-      <div className="flex items-center gap-7">
-        <Link href={`/`}>
-          <div className="flex items-center text-[#14A800] font-extrabold text-xl">
-            ProLink
-          </div>
-        </Link>
-        <div className=" hidden xl:flex gap-6 text-sm font-medium">
-          <Link href={`/freelancer`}>
-            <div className={`${pathname === `/freelancer` && ` underline`}`}>
-              Мэргэжилтэнгүүд
-            </div>
-          </Link>
-          <Link href={`/job`}>
-            <div className={`${pathname === `/job` && ` underline`}`}>
-              Ажлын санал
-            </div>
-          </Link>
-          <Link href={`/client`}>
-            <div className={`${pathname === `/client` && ` underline`}`}>
-              Манайд бүртгэлтэй байгууллагууд
-            </div>
-          </Link>
-        </div>
-      </div>
-      {/* <div className="xl:flex items-center hidden"></div> */}
+	const pathname = usePathname();
+	const getInfo = async () => {
+		const res = await axios.get(`/api/account`);
+		setUserInfo(res.data);
+		await axios.get(`/api/account/refreshToken`);
+	};
+	useEffect(() => {
+		try {
+			getInfo();
+		} catch (err) {
+			console.error(err, "Сервертэй холбогдож чадсангүй!");
+		} finally {
+			setLoading(false);
+		}
+	}, [pathname]);
+	const logout = async () => {
+		setLoading(true);
+		setUserInfo(undefined);
+		const res = await axios.get(`/api/account/logout`);
+		setUserInfo(res.data);
+		setLoading(false);
+	};
+	return (
+		<div className="flex justify-around items-center py-4">
+			<div className="flex items-center gap-7">
+				<Link href={`/`}>
+					<div className="flex items-center text-[#14A800] font-extrabold text-xl">
+						ProLink
+					</div>
+				</Link>
+				<div className=" hidden xl:flex gap-6 text-sm font-medium">
+					<Link href={`/freelancer`}>
+						<div className={`${pathname === `/freelancer` && ` underline`}`}>
+							Мэргэжилтэнгүүд
+						</div>
+					</Link>
+					<Link href={`/job`}>
+						<div className={`${pathname === `/job` && ` underline`}`}>
+							Ажлын санал
+						</div>
+					</Link>
+					<Link href={`/client`}>
+						<div className={`${pathname === `/client` && ` underline`}`}>
+							Манайд бүртгэлтэй байгууллагууд
+						</div>
+					</Link>
+				</div>
+			</div>
+			{/* <div className="xl:flex items-center hidden"></div> */}
 
-      <div className="hidden xl:flex items-center gap-1.5">
-        <div className="flex w-48 border items-center rounded-full px-2">
-          <div className="bg-[#14A800] rounded-full p-1">
-            <IoIosSearch className="text-2xl" />
-          </div>
-          <Input className="border-none shadow-none" placeholder="Хайх" />
-        </div>
+			<div className="hidden xl:flex items-center gap-1.5">
+				<div className="flex w-48 border items-center rounded-full px-2">
+					<div className="bg-[#14A800] rounded-full p-1">
+						<IoIosSearch className="text-2xl" />
+					</div>
+					<Input className="border-none shadow-none" placeholder="Хайх" />
+				</div>
 
-        <div className="flex items-center h-8 rounded-full justify-around gap-2 p-3">
-          {loading ? (
-            <div>Түр хүлээнэ үү...</div>
-          ) : (
-            <div>
-              {response?.code === "SUCCESS" ? (
-                <div className="flex items-center gap-4 whitespace-nowrap">
-                  <Link
-                    className="flex items-center gap-4 whitespace-nowrap"
-                    href={
-                      response.data?.informations?.companyName
-                        ? `/client/${response.data?.informations?.id}`
-                        : `/freelancer/${response.data?.informations?.id}`
-                    }
-                  >
-                    <div className="rounded-full overflow-hidden">
-                      <Image
-                        src={`${response.data?.informations?.pfp}`}
-                        width={30}
-                        height={30}
-                        alt="pfp"
-                      />
-                    </div>
-                    {response.data?.informations?.companyName ? (
-                      <div>{response.data.informations.companyName}</div>
-                    ) : (
-                      <div className=" whitespace-nowrap">
-                        {response.data?.informations?.lastName}{" "}
-                        {response.data?.informations?.firstName}
-                      </div>
-                    )}
-                  </Link>
-                  <button
-                    onClick={logout}
-                    className="  p-0 bg-background hover:bg-secondary cursor-pointer"
-                  >
-                    <RiLogoutBoxRFill className=" text-[#14A800] text-3xl " />
-                  </button>
-                </div>
-              ) : (
-                <Link href={`/account`}>
-                  <Button
-                    sx={{
-                      color: "green",
-                      border: "1px,  solid",
-                      borderRadius: "15px",
-                    }}
-                    className=" cursor-pointer p-2 rounded-2xl px-5 text-background"
-                  >
-                    Нэвтрэх эсвэл бүртгүүлэх
-                  </Button>
-                </Link>
-              )}
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+				<div className="flex items-center h-8 rounded-full justify-around gap-2 p-3">
+					{loading ? (
+						<div>Түр хүлээнэ үү...</div>
+					) : (
+						<div>
+							{response?.code === "SUCCESS" ? (
+								<div className="flex items-center gap-4 whitespace-nowrap">
+									<Link
+										className="flex items-center gap-4 whitespace-nowrap"
+										href={
+											response.data?.informations?.companyName
+												? `/client/${response.data?.informations?.id}`
+												: `/freelancer/${response.data?.informations?.id}`
+										}
+									>
+										<div className="rounded-full overflow-hidden">
+											<Image
+												src={`${response.data?.informations?.pfp}`}
+												width={30}
+												height={30}
+												alt="pfp"
+											/>
+										</div>
+										{response.data?.informations?.companyName ? (
+											<div>{response.data.informations.companyName}</div>
+										) : (
+											<div className=" whitespace-nowrap">
+												{response.data?.informations?.lastName}{" "}
+												{response.data?.informations?.firstName}
+											</div>
+										)}
+									</Link>
+									<button
+										onClick={logout}
+										className="  p-0 bg-background hover:bg-secondary cursor-pointer"
+									>
+										<RiLogoutBoxRFill className=" text-[#14A800] text-3xl " />
+									</button>{" "}
+									{response?.data?.informations?.role == "CLIENT" && (
+										<Link href={`/job/new`}>
+											<button className="bg-[#14A800] rounded-xl p-2 text-white">
+												Post a job
+											</button>
+										</Link>
+									)}
+								</div>
+							) : (
+								<Link href={`/account`}>
+									<Button
+										sx={{
+											color: "green",
+											border: "1px,  solid",
+											borderRadius: "15px",
+										}}
+										className=" cursor-pointer p-2 rounded-2xl px-5 text-background"
+									>
+										Нэвтрэх эсвэл бүртгүүлэх
+									</Button>
+								</Link>
+							)}
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
